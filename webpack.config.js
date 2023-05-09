8811e22339b54a1a51b9bc75842d5db3bda65d9f
Edwin J <hungrylulu8@gmail.com>
2023-05-08 23:25:19 -0700

    using copy-webpack-plugin to copy public/404.html to compiled /dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SocialTags = require('social-tags-webpack-plugin');
 
@@ -100,6 +101,11 @@ module.exports = {
       meta: {
         description,
       },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/404.html', to: '404.html' },
+      ],
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
