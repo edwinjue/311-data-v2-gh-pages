f16a6dbece224e13538cf4be10fc510b2cc838c5
Edwin J <hungrylulu8@gmail.com>
2023-06-05 18:14:43 -0700

    initial HelloWorldWorker component

---
+++ components/duckdb.worker.js
@@ -0,0 +1,9 @@
+onmessage = function (event) {
+  const workerResult = event.data;
+
+  workerResult.onmessage = true;
+
+  console.log('inside duckdb.worker.js, posting. event = ', event);
+  console.log(`the secret message received: ${workerResult?.secretMessage}`);
+  postMessage(workerResult);
+};
