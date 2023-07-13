815588ab6ddc0947a76310ce2db8b19966d5d8f5
Edwin J <hungrylulu8@gmail.com>
2023-07-12 20:10:47 -0700

    added observablehq link in TotalByDayOfWeek widget

--- components/Dashboards/widgets/TotalByDayOfWeek.jsx
+++ components/Dashboards/widgets/TotalByDayOfWeek.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'proptypes';
 const TotalByDayOfWeek = ({ data }) => (
   <>
     <div>Total Requests by Day of The Week</div>
+    {
+      // Observable code goes here
+      // https://observablehq.com/d/8236de092b1f9523#cell-237
+    }
     <ul>
       {data?.map(request => (
         <li key={request.SRNumber}>{request.SRNumber}</li>
