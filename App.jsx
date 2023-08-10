3b7e3508a5a21e49164c684e141102d87bd116fe
Edwin J <hungrylulu8@gmail.com>
2023-08-10 14:49:38 -0700

    removed unused commented swipe code

--- App.jsx
+++ App.jsx
@@ -4,21 +4,11 @@ import { HashRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getMetadataRequest } from '@reducers/metadata';
 import { toggleMenu as reduxToggleMenu } from '@reducers/ui';
-// import { useSwipeable } from 'react-swipeable';
 
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import Routes from './Routes';
 import RouteChange from './components/main/util/RouteChange';
-
-// const menuStyles = {
-//   swipeAreaOpen: {
-//     float: 'left',
-//     position: 'fixed',
-//     width: 150,
-//     height: '100%',
-//   },
-// };
 
 const App = ({
   getMetadata,
@@ -28,19 +18,10 @@ const App = ({
     getMetadata();
   });
 
-  // const handleSwipeMenu = useSwipeable({
-  //   trackMouse: true,
-  //   onSwipedRight: () => toggleMenu(),
-  //   onSwipedLeft: () => toggleMenu(),
-  // });
-
   return (
     <HashRouter>
       <Header />
       <Routes />
-      {/* area where you can swipe the menu sidebar */}
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      {/* <div {...handleSwipeMenu} style={menuStyles.swipeAreaOpen} /> */}
       <Footer />
       <RouteChange />
     </HashRouter>
