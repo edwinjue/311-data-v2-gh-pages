82ff70add8e3f1fff6b90fe1a5f5062b6c31b0fa
Edwin J <hungrylulu8@gmail.com>
2023-08-11 14:01:30 -0700

    removed duckdb.worker.js

--- components/duckdb.worker.js
+++
@@ -1,9 +0,0 @@
-onmessage = function (event) {
-  const workerResult = event.data;
-
-  workerResult.onmessage = true;
-
-  console.log('inside duckdb.worker.js, posting. event = ', event);
-  console.log(`the secret message received: ${workerResult?.secretMessage}`);
-  postMessage(workerResult);
-};
